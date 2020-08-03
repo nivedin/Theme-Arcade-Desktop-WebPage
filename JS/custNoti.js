c2b0e04e1578e6c8826a86f77e36fd4f5e34c0a4
Nivedin <nivedinp@gmail.com>
2020-08-03 22:10:06 +0530

    fix noti 1

--- JS/custNoti.js
+++ JS/custNoti.js
@@ -1,1 +1,33 @@
 AOS.init();
+
+let showMore = document.querySelector(".showMore");
+let showMoreContent = document.querySelector(".showMrCont");
+
+let showMrClicked = () => {
+  showMoreContent.classList.remove("showMrDisp");
+  showMore.innerHTML = "";
+};
+
+showMore.addEventListener("click", showMrClicked);
+
+let scrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});

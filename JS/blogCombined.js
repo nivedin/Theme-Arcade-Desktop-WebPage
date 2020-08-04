c652254c2eb0acb22a1d32473c70f7b28df0aaff
Nivedin <nivedinp@gmail.com>
2020-08-04 21:49:27 +0530

    fix blogCombined 1

--- JS/blogCombined.js
+++ JS/blogCombined.js
@@ -2,10 +2,32 @@ AOS.init();
 
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 4,
-  spaceBetween: 5,
+
   grabCursor: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+let scrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });

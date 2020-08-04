a1039e324fb1ff75ea239f0049e886850d9296ea
Nivedin <nivedinp@gmail.com>
2020-08-04 21:01:44 +0530

    fix support 1

--- JS/support.js
+++ JS/support.js
@@ -1,1 +1,23 @@
 AOS.init();
+
+let scrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});

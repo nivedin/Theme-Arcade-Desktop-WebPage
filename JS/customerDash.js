ac07718b152460ac7b8eb68ebf0bf55519d40900
Nivedin <nivedinp@gmail.com>
2020-08-05 15:25:38 +0530

    fix custDash 1

--- JS/customerDash.js
+++ JS/customerDash.js
@@ -1,1 +1,36 @@
 AOS.init();
+let scrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+let editBtn = document.querySelector(".editBtn");
+let inputBars = document.querySelectorAll(
+  ".persDetailMain .editableFeild input"
+);
+
+let editClicked = () => {
+  console.log("clicked");
+  inputBars.forEach((inputBar) => {
+    inputBar.removeAttribute("readonly");
+  });
+};
+
+editBtn.addEventListener("click", editClicked);

f0f8ac302c0c9e7296fdcdc33d3d0c2af70f45cd
Nivedin <nivedinp@gmail.com>
2020-08-05 15:47:54 +0530

    fix landing toTop

--- JS/landing.js
+++ JS/landing.js
@@ -24,3 +24,25 @@ jQuery(document).ready(function ($) {
     time: 1000,
   });
 });
+
+let scrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});

51e9d0cc01efd399f4f2d16cc8cfe773970bc58e
Nivedin <nivedinp@gmail.com>
2020-08-05 14:35:34 +0530

    fix blogIndividual 1

--- JS/blogIndividual.js
+++ JS/blogIndividual.js
@@ -1,12 +1,37 @@
 AOS.init();
 
 let likeButton = document.querySelector(".heartLikeBtn");
+let likeButtonIcon = document.querySelector(".heartLikeBtn .fa-heart");
 let likeNumber = document.querySelector(".likeNumber");
 
 let totalLike = 201;
 
-let likeClicked = () => {
+likeButton.addEventListener("click", function () {
   likeNumber.innerHTML = totalLike++;
+  this.children[0].classList.remove("far");
+  this.children[0].classList.add("fas");
+  this.children[0].style.color = "red";
+  this.firstElementChild.style.transition = "all 1s ease 0.3s";
+});
+
+let scrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.onscroll = function () {
+  scrollFunction();
 };
 
-likeButton.addEventListener("click", likeClicked);
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});

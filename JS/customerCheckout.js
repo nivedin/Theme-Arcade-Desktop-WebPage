d6911e5ce2576d49d1736a922e0fbc1b992d3483
Nivedin <nivedinp@gmail.com>
2020-08-14 21:16:00 +0530

    added customer Checkout

---
+++ JS/customerCheckout.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+let cartItems = document.querySelectorAll(".product");
+let removeItemButtons = document.querySelectorAll(".removeCloseBtn");
+let viewMrContent = document.querySelector(".viewMrClicked");
+let viewMrBtn = document.querySelector(".viewMr");
+
+//Close Clicked
+for (var i = 0; i < removeItemButtons.length; i++) {
+  var removeButton = removeItemButtons[i];
+  removeButton.addEventListener("click", function (event) {
+    var removeBtnClicked = event.target;
+    removeBtnClicked.parentElement.parentElement.style.animation =
+      "itemAnim 5s ease-in-out forwards";
+    removeBtnClicked.parentElement.parentElement.remove();
+    cartItems.length--;
+  });
+}
+
+viewMrBtn.addEventListener("click", () => {
+  viewMrContent.style.display = "block";
+  viewMrBtn.remove();
+});
